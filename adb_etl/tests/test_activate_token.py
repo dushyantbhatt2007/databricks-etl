610472f0335bf21f687a586548aa01e7ea4bfc1a
Dushyant Bhatt <dubhatt@microsoft.com>
2020-05-18 10:09:52 -0700

    config changes.

--- adb_etl/tests/test_activate_token.py
+++ adb_etl/tests/test_activate_token.py
@@ -4,3 +4,10 @@ from pyspark.dbutils import DBUtils
 spark = SparkSession.builder.appName("test").master("local[*]").getOrCreate()
 dbutils = DBUtils(spark.sparkContext)
 dbutils.secrets.setToken("")
+
+'''
+// command to get DBUtils token
+displayHTML(
+  "<b>Privileged DBUtils token (expires in 48 hours): </b>" +
+  dbutils.notebook.getContext.apiToken.get.split("").mkString("<span/>"))
+'''
--- setup.py
+++ setup.py
@@ -65,7 +65,6 @@ setup(
     install_requires=REQUIRED,
     extras_require=EXTRAS,
     include_package_data=True,
-    license='Microsoft',
     classifiers=[
         # Trove classifiers
         # Full list: https://pypi.python.org/pypi?%3Aaction=list_classifiers
